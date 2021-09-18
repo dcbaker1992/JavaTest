46171bdcb07da38a7acc3cda46bd4585f562455a
Dillon <dcbaker.1992@gmail.com>
2021-09-18 12:22:27 -0500

    Test Java install

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld  {
+    //HelloWorld(class name) must match the file name
+    public static void main(String[] args){
+    //must have the about line in any test file in order for the code to run 
+
+        System.out.println("Hello World!");
+        System.out.println("Hiya Friend!");
+    }
+}
